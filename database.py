a512264d7d990030c966e3dd33177bc4fd1f7eeb
Rafael Khazivaliev <rafael.khazivaliev@gmail.com>
2025-08-11 03:02:00 +0300

    feat: заменил асинхронный движок на синхронный для поддержки Celery (database.py)

--- database.py
+++ database.py
@@ -2,19 +2,20 @@
 Модуль для настройки подключения к базе данных с использованием SQLAlchemy и asyncpg.
 """
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres@localhost/postgres"
+DATABASE_URL = "postgresql+pg8000://postgres@localhost/postgres"
 
-engine = create_async_engine(
+engine = create_engine(
     DATABASE_URL,
     echo=True,
-    pool_size=5,           # можно уменьшить/увеличить в зависимости от ресурсов
-    max_overflow=0,        # чтобы не создавать новых сверх пула
-    pool_pre_ping=True     # проверяет соединение перед использованием
+    pool_size=5,  # можно уменьшить/увеличить в зависимости от ресурсов
+    max_overflow=0,  # чтобы не создавать новых сверх пула
+    pool_pre_ping=True,  # проверяет соединение перед использованием
 )
 
-AsyncSessionLocal = async_sessionmaker(
+SessionLocal = sessionmaker(
     bind=engine,
     expire_on_commit=False,
 )
